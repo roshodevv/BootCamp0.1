d85187139e3f31b9afbc08312194a50a88ba80cd
Rush <itzashert@gmail.com>
2026-02-03 20:12:11 +0530

    Passed

--- Day02 - Async JS/callbacks/easy/fetchWithTimeout.js
+++ Day02 - Async JS/callbacks/easy/fetchWithTimeout.js
@@ -5,8 +5,33 @@
 // If the request completes within the specified time, the Promise resolves with the fetched data. 
 // If the operation exceeds the time limit, the Promise rejects with the message "Request Timed Out".
 
-function fetchWithTimeout(url, ms) {}
+/**
+ * Set up two competeing Promises 
+ * first returns a fetch Promise
+ * second promise returns reject on time (ms) completion
+ * finally we use Promise.race([o1 , o2]), whichever resolves first is returned!
+ */
+function fetchWithTimeout(url, ms) {
+  const fetchPromise = fetch(url);
 
-function fetchWithTimeoutClean(url, ms) {}
+  const timeoutPromise = new Promise((_, reject) => {
+    setTimeout(() => {
+      reject("Request Timed Out");
+    }, ms);
+  });
+
+  return Promise.race([fetchPromise, timeoutPromise]);
+}
+
+function fetchWithTimeoutClean(url, ms) {
+  return Promise.race([
+    fetch(url),
+    new Promise((_, reject) =>
+      setTimeout(() => reject("Request Timed Out"), ms)
+    )
+  ]);
+}
 
 module.exports = { fetchWithTimeout, fetchWithTimeoutClean };
+
+// PASSED //
