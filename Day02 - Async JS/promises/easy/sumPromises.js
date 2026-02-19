eacfe4d23ed31a9f57f7af15c5c26a79aab2db72
Rush <itzashert@gmail.com>
2026-02-19 20:33:36 +0530

    PASSED

--- Day02 - Async JS/promises/easy/sumPromises.js
+++ Day02 - Async JS/promises/easy/sumPromises.js
@@ -3,6 +3,22 @@
 // You are given two Promises that each resolve to numeric values. 
 // Your task is to return a new Promise that resolves to the sum of these two numbers. 
 // Both Promises should be executed in parallel using Promise.all to avoid unnecessary waiting.
-async function sumPromises(p1, p2) {}
+
+
+async function sumPromises(p1, p2) {  // since async -> automaticall returns Promise !! 
+        try{
+            // Run both the promises using Promise.all
+            let [a, b] = await Promise.all([p1, p2]);
+
+            // Add both the values
+            // return the sum 
+            return a+b;
+        }
+        catch(err){
+            return new Error("error");
+        }
+}
 
 module.exports = sumPromises;
+
+// PASSED 
